91e7e286c4014d52397538094ef66544923f3af3
raisart <aseven77@mail.ru>
2022-09-06 10:27:01 +0500

    fix

---
+++ dist/index.c50b2688.js
@@ -0,0 +1,2 @@
+document.querySelector("#root").textContent=function(...t){if(0===t.length)throw Error("sum required at least 1 argument");return t.reduce(((t,e)=>t+e),0)}(8,9).toString();
+//# sourceMappingURL=index.c50b2688.js.map
